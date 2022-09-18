7b99bc5ef683c61a498411ca96953274e9e2e806
Sarah Sami <sarah7sami@gmail.com>
2022-09-18 13:07:07 -0400

    answ 1 added

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 // create a function 
 
-// Ex1. Rewrite the following code block using ES6 syntax, ie. const, let, arrow function, template literals
+// function greeter(myArray, counter) {
+//     var greetText = "Hello ";
+
+//     for (var index = 0; index < myArray.length; index++) {
+//         console.log(greetText + myArray[index]);
+//     }
+// }
+
+// Ex1. Rewrite the above code block using ES6 syntax, ie. const, let, arrow function, template literals
 // and for..of
 
-function greeter(myArray, counter) {
-    var greetText = "Hello ";
-
-    for (var index = 0; index < myArray.length; index++) {
-        console.log(greetText + myArray[index]);
+const greeter = (myArray) => {
+    let greetText = "Hello ";
+    for (const name of myArray) {
+        console.log(`${greetText} ${name}`);
     }
 }
 
 // Test Ex1.
-console.log(greeter(['Sarah Sami', 'Pritesh Patel'], 2))
+console.log(greeter(['Sarah Sami', 'Pritesh Patel']));
+
+
 
